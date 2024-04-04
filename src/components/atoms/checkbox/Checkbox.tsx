9a4e7bf522890a42a2116c1555fbfd7203e0c78e
김 연서 <yeonseo341@gmail.com>
2024-04-04 04:12:28 +0000

    fix: 주석 수정

--- src/components/atoms/checkbox/Checkbox.tsx
+++ src/components/atoms/checkbox/Checkbox.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 type CheckboxProps = {
   checked: boolean;
   onChange: (checked: boolean) => void;
-  color?: string; // 색상을 선택적으로 받습니다
-  size?: number; // 크기를 선택적으로 받습니다
+  color?: string; 
+  size?: number; 
 };
 
 const StyledCheckbox = styled.input.attrs({ type: 'checkbox' })`
